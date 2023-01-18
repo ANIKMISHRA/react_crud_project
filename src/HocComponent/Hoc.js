561685320ff21cffba5c1aff066c25f62cf5072f
anikmishra836 <anikmishra836@gmail.com>
2023-01-18 11:35:05 +0530

    testing

--- src/HocComponent/Hoc.js
+++ src/HocComponent/Hoc.js
@@ -1,37 +1,31 @@
+// npm packages
 import React from "react";
 import { useSelector } from "react-redux";
+
+// component
 import Home from "../Pages/Home";
+
+// services
 import { USER_NOT_LOGGINED_MESSAGE } from "../Services/Constants/Messages";
 import { popupMessages } from "../Services/popupMessages";
 
-// components
 
 const hocComponent = (OriginalComponent, isHome = false) => {
   return (props) => {
-    
-   // const loggedInUser = false;
-   const loggedInUser = useSelector((state) => state?.user);
-
+    const loggedInUser = useSelector((state) => state?.user);
 
     const renderComponent = () => {
-      if(isHome) {
-        return (
-          <OriginalComponent loggedInUser={loggedInUser} />
-        );
+      if (isHome) {
+        return <OriginalComponent loggedInUser={loggedInUser} />;
       }
 
-      if(loggedInUser) {
-        return (
-          <OriginalComponent {...props} />
-        )
+      if (loggedInUser) {
+        return <OriginalComponent {...props} />;
       } else {
-        return (
-          popupMessages(USER_NOT_LOGGINED_MESSAGE),
-          <Home />
-        )
+        return popupMessages(USER_NOT_LOGGINED_MESSAGE), (<Home />);
       }
-    }
+    };
     return <>{renderComponent()}</>;
-  }
+  };
 };
 export default hocComponent;
